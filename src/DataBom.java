42dc52513dbe189392f1c10023494fcbf93f218d
Oleg <kio5885@gmail.com>
2021-09-15 17:08:44 +0700

    Исправил чтение значений резисторов и конденсаторов

--- src/DataBom.java
+++ src/DataBom.java
@@ -16,16 +16,21 @@ public class DataBom {
     String type;
     String solderType;
     int amountPad;
+    String comment;
     double sortWeightValue;
     int sortWeightLayer;
     int sortWeightFootprint;
     int sortWeightType;
 
-    public DataBom(Sheet sheet,int numRow ) throws IOException {
+    public DataBom() {
+    }
+
+
+    public DataBom(Sheet sheet, int numRow) throws IOException {
 
         componentBase = new ComponentBase("ComponentBase.xlsx");
         //Прочитать Строку
-        sheetReadBom    =   sheet;
+        sheetReadBom = sheet;
         if (sheetReadBom.getRow(numRow + 1) == null) {
             sheetReadBom.createRow(numRow + 1);
         }
@@ -46,50 +51,170 @@ public class DataBom {
 
     }
 
+    public void convertValueCapacitor(String convertValue) {
 
+        //Необходимо вырезать символ F из значения
+        if (convertValue.contains("F")) {
+            int i = convertValue.indexOf("F");
+            convertValue = convertValue.substring(0, i) + convertValue.substring(i + 1);
+        }
+
+        String[] splitValue = convertValue.split(" ");
+        String capValue = "";
+        String commentValue = "";
+        String[] splitOmValue;
+
+
+        for (int i = 0; i < splitValue.length; i++) {
+            //Первый член всегда значение сопротивления
+            if (i == 0) {
+                capValue = splitValue[i];
+            } else {
+                commentValue = commentValue + "," + splitValue[i];
+                comment =   commentValue;
+            }
+        }
+
+
+        if (capValue.contains("u")) {
+            splitOmValue = capValue.split("u");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                sortWeightValue *= 1E-6;
+                //Точка являеться разделителем дробной части
+            } else if (capValue.contains(".")) {
+                //Нужно отбросить префикс
+                capValue = capValue.substring(0, capValue.indexOf("u"));
+                sortWeightValue = Double.parseDouble(capValue);
+                sortWeightValue *= 1E-6;
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+                sortWeightValue *= 1E-6;
+            }
+        } else if (capValue.contains("n")) {
+            splitOmValue = capValue.split("n");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                sortWeightValue *= 1E-9;
+                //Точка являеться разделителем дробной части
+            } else if (capValue.contains(".")) {
+                //Нужно отбросить префикс
+                capValue = capValue.substring(0, capValue.indexOf("n"));
+                sortWeightValue = Double.parseDouble(capValue);
+                sortWeightValue *= 1E-9;
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+                sortWeightValue *= 1E-9;
+            }
+        } else if (capValue.contains("p")) {
+            splitOmValue = capValue.split("p");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                sortWeightValue *= 1E-12;
+                //Точка являеться разделителем дробной части
+            } else if (capValue.contains(".")) {
+                //Нужно отбросить префикс
+                capValue = capValue.substring(0, capValue.indexOf("p"));
+                sortWeightValue = Double.parseDouble(capValue);
+                sortWeightValue *= 1E-12;
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+                sortWeightValue *= 1E-12;
+            }
+        } else if (capValue.contains(".")) {
+            sortWeightValue = Double.parseDouble(capValue);
+        } else {
+            System.out.println("Ошибка convertValueResistor");
+        }
+
+    }
+
+    public void convertValueResistor(String convertValue) {
+        String[] splitValue = convertValue.split(" ");
+        String omValue = "";
+        String commentValue = "";
+        String[] splitOmValue;
+
+
+        for (int i = 0; i < splitValue.length; i++) {
+            //Первый член всегда значение сопротивления
+            if (i == 0) {
+                omValue = splitValue[i];
+            } else {
+                commentValue = commentValue + "," + splitValue[i];
+                comment = commentValue;
+            }
+        }
+
+        if (omValue.contains("R")) {
+            splitOmValue = omValue.split("R");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                //Точка являеться разделителем дробной части
+            } else if (omValue.contains(".")) {
+                //Нужно отбросить префикс
+                omValue = omValue.substring(0, omValue.indexOf("R"));
+                sortWeightValue = Double.parseDouble(omValue);
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+            }
+        } else if (omValue.contains("k")) {
+            splitOmValue = omValue.split("k");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                sortWeightValue *= 1E3;
+                //Точка являеться разделителем дробной части
+            } else if (omValue.contains(".")) {
+                //Нужно отбросить префикс
+                omValue = omValue.substring(0, omValue.indexOf("k"));
+                sortWeightValue = Double.parseDouble(omValue);
+                sortWeightValue *= 1E3;
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+                sortWeightValue *= 1E3;
+            }
+        } else if (omValue.contains("M")) {
+            splitOmValue = omValue.split("M");
+            //R являеться разделителем дробной части
+            if (splitOmValue.length >= 2) {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]) + (Double.parseDouble(splitOmValue[1]) / 10);
+                sortWeightValue *= 1E6;
+                //Точка являеться разделителем дробной части
+            } else if (omValue.contains(".")) {
+                //Нужно отбросить префикс
+                omValue = omValue.substring(0, omValue.indexOf("M"));
+                sortWeightValue = Double.parseDouble(omValue);
+                sortWeightValue *= 1E6;
+                //Не нашли дробную часть
+            } else {
+                sortWeightValue = Double.parseDouble(splitOmValue[0]);
+                sortWeightValue *= 1E6;
+            }
+        } else if (omValue.contains(".")) {
+            sortWeightValue = Double.parseDouble(omValue);
+        } else {
+            System.out.println("Ошибка convertValueResistor");
+        }
+    }
 
     private void setSortWeightValue() {
 
-        if (type.equals("Resistor")) {
-            if (value.contains("R")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("R")));
-                if (Double.parseDouble(value.substring(value.indexOf("R") + 1))>0) {
-
-                    double tentsDoubleValue = Double.parseDouble(value.substring(value.indexOf("R") + 1)) / 10;
-                    sortWeightValue = sortWeightValue + tentsDoubleValue;
-
-                }
-            } else if (value.contains("k")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("k")));
-                //TODO не работает расчет
-            } else if (value.contains("M")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("M")));
-                if (value.length() > value.indexOf("M")) {
-
-                    double tentsDoubleValue = Double.parseDouble(value.substring(value.indexOf("M") + 1)) / 10;
-                    sortWeightValue = (sortWeightValue + tentsDoubleValue) * 1000000;
-
-                }
-            } else {
-                sortWeightValue =Double.parseDouble(value);
-                System.out.println("Неизвестный префикс резистора");
-            }
-        } else if (type.equals("Capacitor")) {
-            if (value.contains("u")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("u"))) * 1E-6;
-
-            } else if (value.contains("n")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("n"))) * 1E-9;
-
-            } else if (value.contains("p")) {
-                sortWeightValue = Double.parseDouble(value.substring(0, value.indexOf("p"))) * 1E-12;
-
-            } else {
-                sortWeightValue = Double.parseDouble(value);
-                System.out.println("Неизвестный префикс резистора");
-            }
+        if (type.equals("Resistor")){
+            convertValueResistor(value);
+        }  else if (type.equals("Capacitor")) {
+            convertValueCapacitor(value);
         } else {
-            System.out.println("не ковертируемый тип данных"+type);
+            System.out.println("не ковертируемый тип данных" + type);
             sortWeightValue = 0;
         }
     }
@@ -111,7 +236,7 @@ public class DataBom {
         tableFootprint[12] = "R0402";
         tableFootprint[13] = "No Footprint";
 
-        sortWeightFootprint = tableFootprint.length-1;
+        sortWeightFootprint = tableFootprint.length - 1;
 
         for (int i = 0; i < tableFootprint.length; i++) {
             if (footprint.equals(tableFootprint[i])) {
@@ -121,7 +246,7 @@ public class DataBom {
 
     }
 
-    public void setSortWeightType(){
+    public void setSortWeightType() {
         String[] tableType = new String[7];
         tableType[0] = "Resistor";
         tableType[1] = "Capacitor";
@@ -132,24 +257,24 @@ public class DataBom {
         tableType[6] = "No Type";
 
 
-        sortWeightType  =   tableType.length-1;
+        sortWeightType = tableType.length - 1;
         for (int i = 0; i < tableType.length; i++) {
-            if(type.equals(tableType[i])){
-                sortWeightType  =   i;
+            if (type.equals(tableType[i])) {
+                sortWeightType = i;
             }
         }
     }
 
-    public void setSortWeightLayer(){
+    public void setSortWeightLayer() {
         String[] tableType = new String[6];
         tableType[0] = "T";
         tableType[1] = "B";
         tableType[2] = "No Type";
 
-        sortWeightLayer  =   tableType.length-1;
+        sortWeightLayer = tableType.length - 1;
         for (int i = 0; i < tableType.length; i++) {
-            if(layer.equals(tableType[i])){
-                sortWeightLayer  =   i;
+            if (layer.equals(tableType[i])) {
+                sortWeightLayer = i;
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,44 @@ public class Main {
 
     public static void main(String[] args) throws EncryptedDocumentException, IOException {
 
-        String people = "8k25 10%";
 
-        String[] peopleArray = people.split(" ");
-        for (String human : peopleArray) {
-            System.out.println(human);
-            if (human.contains("k")){
-                String[] testArray = human.split("k");
-                for (String test : testArray) {
-                    System.out.println(test);
+        DataBom test  =   new DataBom();
 
-                }
-            }
+        test.convertValueCapacitor("1.2nF");
+
+//        String people = "8k25 10%";
+//
+//        String[] peopleArray = people.split(" ");
+//        for (String human : peopleArray) {
+//            System.out.println(human);
+//            if (human.contains("k")){
+//                String[] testArray = human.split("k");
+//                for (String test : testArray) {
+//                    System.out.println(test);
+//
+//                }
+//            }
+//
+//
+//        }
+//        System.out.println(peopleArray.length);
+
+      Bom bom =   new Bom("Test_BOM.xlsx");
 
 
-        }
-        System.out.println(peopleArray.length);
 
-//        Bom bom =   new Bom("Test_BOM.xlsx");
-//
-//        bom.readComponent   = bom.readAllComponents();
-//
-//        bom.sortBom(bom.readComponent);
-//
-//       // bom.readComponent   = bom.contEqualsRows(bom.readComponent);
-//
-//        bom.saveAllComponents("Sort_BOM.xlsx",bom.readComponent);
-//
-//
-//        System.out.println("End");
-//        System.out.println("ENd");
+
+        bom.readComponent   = bom.readAllComponents();
+
+        bom.sortBom(bom.readComponent);
+
+       // bom.readComponent   = bom.contEqualsRows(bom.readComponent);
+
+        bom.saveAllComponents("Sort_BOM.xlsx",bom.readComponent);
+
+
+        System.out.println("End");
+        System.out.println("ENd");
 
 
     }
